38b8250164703a27c3d0e40003f5b1e7446d6dc4
MrSPD-2703 <59143264+MrSPD-2703@users.noreply.github.com>
2020-05-24 02:23:58 +0530

    🐱‍👤📱🔋🔌

--- app/src/main/java/com/mrspd/custombatterylevelcontroller/MainActivity.kt
+++ app/src/main/java/com/mrspd/custombatterylevelcontroller/MainActivity.kt
@@ -1,44 +1,40 @@
 package com.mrspd.custombatterylevelcontroller
-
-import android.annotation.SuppressLint
-import android.annotation.TargetApi
-import android.app.Notification
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.graphics.BitmapFactory
-import android.graphics.Color
-import android.os.BatteryManager
-import android.os.Build
-import android.os.Bundle
-import android.util.Log.d
-import android.view.View
-import android.widget.*
-import android.widget.SeekBar.OnSeekBarChangeListener
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import java.lang.Exception
 
-
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
-
-
-    lateinit var TV: TextView
-    lateinit var S: SeekBar
-    lateinit var TB: ToggleButton
-    lateinit var B1: Button
-    var B2: android.widget.Button? = null
-    var B3: android.widget.Button? = null
+    var textView: TextView? = null
+    var seekBAr: SeekBar? = null
+    var toggleButton: ToggleButton? = null
+    var button1: Button? = null
+    var button2: Button? = null
+    var button3: Button? = null
     var i = 0
-    var Level: Int = 0
-    var L: Int = 0
-    lateinit var T3: TextView
-    var I: ImageView? = null
-
+    var Level = 0
+    var L = 0
+    var textView1: TextView? = null
+    var imageView: ImageView? = null
     private val b: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             Level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
@@ -49,36 +45,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.registerReceiver(b, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-        TV = findViewById<TextView>(R.id.textView)
-        S = findViewById<SeekBar>(R.id.seekBar)
-        TB = findViewById<ToggleButton>(R.id.toggleButton)
-        B1 = findViewById<Button>(R.id.button)
-        B2 = this.findViewById<View>(R.id.button2) as Button
-        B3 = findViewById<View>(R.id.button3) as Button
-        I = findViewById<ImageView>(R.id.imageView)
-        T3 = findViewById<TextView>(R.id.textView3)
-        S.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
-            @SuppressLint("SetTextI18n")
+        textView = findViewById<View>(R.id.textView) as TextView
+        seekBAr = findViewById<View>(R.id.seekBar) as SeekBar
+        toggleButton = findViewById<View>(R.id.toggleButton) as ToggleButton
+        button1 = findViewById<View>(R.id.button) as Button
+        button2 = findViewById<View>(R.id.button2) as Button
+        button3 = findViewById<View>(R.id.button3) as Button
+        imageView = findViewById<View>(R.id.imageView) as ImageView
+        textView1 = findViewById<View>(R.id.textView3) as TextView
+        seekBAr!!.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, P: Int, b: Boolean) {
-                TV.text = "$P%"
+                textView!!.text = "$P%"
                 L = P
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
             override fun onStopTrackingTouch(seekBar: SeekBar) {
-                T3.visibility = View.INVISIBLE
-                TB.visibility = View.VISIBLE
-                B1.visibility = View.VISIBLE
+                textView1!!.visibility = View.INVISIBLE
+                toggleButton!!.visibility = View.VISIBLE
+                button1!!.visibility = View.VISIBLE
             }
         })
-    } //end of on create
+    }
+
     override fun onRestart() {
         super.onRestart()
         try {
             unregisterReceiver(b)
         }
-        catch (e : Exception){
-            d("GG","Exception Found")
+        catch (e:Exception){
+
         }
 
     }
@@ -94,15 +90,15 @@ class MainActivity : AppCompatActivity() {
         `in`.addCategory(Intent.CATEGORY_HOME)
         this.startActivity(`in`)
         i = 1
-        Toast.makeText(this, "ALarm Will Start At " + TV.text + " %", Toast.LENGTH_SHORT).show()
-        val notificationBuilder = NotificationCompat.Builder(this)
+        Toast.makeText(this@MainActivity, "ALarm Will Start At " + textView!!.text + " %", Toast.LENGTH_SHORT).show()
+        val notificationBuilder = NotificationCompat.Builder(this@MainActivity)
                 .setSmallIcon(R.drawable.power)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.power))
         notificationBuilder.setDefaults(Notification.DEFAULT_LIGHTS or Notification.DEFAULT_VIBRATE or Notification.DEFAULT_SOUND)
-        val notificationManager = NotificationManagerCompat.from(this)
-        B1.visibility = View.INVISIBLE
-        B2!!.visibility = View.VISIBLE
-        val t: Thread = object : Thread() {
+        val notificationManager = NotificationManagerCompat.from(this@MainActivity)
+        button1!!.visibility = View.INVISIBLE
+        button2!!.visibility = View.VISIBLE
+        val thread: Thread = object : Thread() {
             @TargetApi(Build.VERSION_CODES.M)
             @RequiresApi(api = Build.VERSION_CODES.M)
             override fun run() {
@@ -110,31 +106,31 @@ class MainActivity : AppCompatActivity() {
                     while (!isInterrupted) {
                         sleep(500)
                         runOnUiThread {
-                            if (i == 1 && Level >= L && !TB.isChecked) {
+                            if (i == 1 && Level >= L && !toggleButton!!.isChecked) {
                                 notificationBuilder.setContentText("Battery Level Exceeding")
                                 notificationManager.notify(1, notificationBuilder.build())
                                 notificationBuilder.priority = Notification.PRIORITY_HIGH
-                                TV.visibility = View.INVISIBLE
-                                S.visibility = View.INVISIBLE
-                                TB.visibility = View.INVISIBLE
-                                I!!.visibility = View.VISIBLE
-                                B3!!.visibility = View.VISIBLE
-                                B2!!.text = "Stop"
-                                B2!!.visibility = View.VISIBLE
-                                B2!!.setBackgroundColor(Color.parseColor("#00FF00"))
+                                textView!!.visibility = View.INVISIBLE
+                                seekBAr!!.visibility = View.INVISIBLE
+                                toggleButton!!.visibility = View.INVISIBLE
+                                imageView!!.visibility = View.VISIBLE
+                                button3!!.visibility = View.VISIBLE
+                                button2!!.text = "Stop"
+                                button2!!.visibility = View.VISIBLE
+                                button2!!.setBackgroundColor(Color.parseColor("#00FF00"))
                             }
-                            if (i == 1 && Level <= L && TB.isChecked) {
+                            if (i == 1 && Level <= L && toggleButton!!.isChecked) {
                                 notificationBuilder.setContentText("Battery Level Depleting")
                                 notificationManager.notify(1, notificationBuilder.build())
                                 notificationBuilder.priority = Notification.PRIORITY_HIGH
-                                TV.visibility = View.INVISIBLE
-                                S.visibility = View.INVISIBLE
-                                TB.visibility = View.INVISIBLE
-                                I!!.visibility = View.VISIBLE
-                                B3!!.visibility = View.VISIBLE
-                                B2!!.text = "Stop"
-                                B2!!.visibility = View.VISIBLE
-                                B2!!.setBackgroundColor(Color.parseColor("#00FF00"))
+                                textView!!.visibility = View.INVISIBLE
+                                seekBAr!!.visibility = View.INVISIBLE
+                                toggleButton!!.visibility = View.INVISIBLE
+                                imageView!!.visibility = View.VISIBLE
+                                button3!!.visibility = View.VISIBLE
+                                button2!!.text = "Stop"
+                                button2!!.visibility = View.VISIBLE
+                                button2!!.setBackgroundColor(Color.parseColor("#00FF00"))
                             }
                         }
                         sleep(10000)
@@ -143,25 +139,24 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        t.start()
+        thread.start()
     }
-
 
     fun OFF(view: View?) {
         i = 0
-        B3!!.visibility = View.INVISIBLE
-        B1.visibility = View.VISIBLE
-        I!!.visibility = View.INVISIBLE
-        TV.visibility = View.VISIBLE
-        S.visibility = View.VISIBLE
-        TB.visibility = View.VISIBLE
-        B2!!.text = "OFF"
-        B2!!.setBackgroundColor(Color.parseColor("#00BCD4"))
-        B2!!.visibility = View.INVISIBLE
-        S.visibility = View.VISIBLE
-        TB.visibility = View.INVISIBLE
-        B1.visibility = View.INVISIBLE
-        T3.visibility = View.VISIBLE
+        button3!!.visibility = View.INVISIBLE
+        button1!!.visibility = View.VISIBLE
+        imageView!!.visibility = View.INVISIBLE
+        textView!!.visibility = View.VISIBLE
+        seekBAr!!.visibility = View.VISIBLE
+        toggleButton!!.visibility = View.VISIBLE
+        button2!!.text = "OFF"
+        button2!!.setBackgroundColor(Color.parseColor("#00BCD4"))
+        button2!!.visibility = View.INVISIBLE
+        seekBAr!!.visibility = View.VISIBLE
+        toggleButton!!.visibility = View.INVISIBLE
+        button1!!.visibility = View.INVISIBLE
+        textView1!!.visibility = View.VISIBLE
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
